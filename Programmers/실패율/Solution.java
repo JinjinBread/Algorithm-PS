db5243c40952cca73da71a40c5cf4846cbaeca79
jieun <92997924+JinjinBread@users.noreply.github.com>
2024-07-07 23:59:40 +0900

    study11: Programmers Lv.1 실패율 리팩토링

--- Programmers/실패율/Solution.java
+++ Programmers/실패율/Solution.java
@@ -10,49 +10,33 @@ class Solution {
         // 스테이지에 도달한 플레이어 수(즉 1보다 크거나 같은 스테이지 수)
         // 2스테이지 - 3/7, 3스테이지 - 1/2, 4스테이지 - 1/2, 5스테이지 - 0
         // 3 - 4 - 2 - 1 - 5
-        List<Double> result = new ArrayList<>();
-        
-        Arrays.sort(stages);
-        
-        for (int i = 0; i < N; i++) {
-            double challenger = 0.0;
-            double passer = 0.0;
-            for (int j = 0; j < stages.length; j++) {
-                int stage = stages[j];
-                
-                //스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수
-                if (stage == (i + 1))
-                    challenger++;
-                
-                //스테이지에 도달한 플레이어의 수
-                if (stage >= (i + 1))
-                    passer++;
-            }
-            
+
+        int[] challenger = new int[N + 2]; //1 ~ N+1 스테이지 별 도전자를 구함
+
+        for (int i = 0; i < stages.length; i++) {
+            challenger[stages[i]]++;
+        }
+
+        double passer = stages.length; // 스테이지 도달한 플레이어 수
+
+        Map<Integer, Double> failure = new HashMap<>();
+
+        for (int i = 1; i <= N; i++) {
+
             //스테이지에 도달한 유저가 없는 경우
-            if (passer == 0) {
-                result.add(0.0);
+            if (challenger[i] == 0) {
+                failure.put(i, 0.);
                 continue;
             }
-            
-            result.add(challenger/passer);
+
+            failure.put(i, challenger[i] / passer);
+            passer -= challenger[i];
         }
         
-        int[] answer = new int[N];
-        
-        for(int i = 0; i < answer.length; i++) {
-            
-            double max = Collections.max(result);
-            
-            for(int j = 0; j < result.size(); j++) {
-                if (result.get(j) == max) {
-                    answer[i] = j + 1;
-                    result.set(j, -1.0);
-                    break;
-                }
-            }
-        }
-        
-        return answer;
+        //내림차순 정렬
+        return failure.entrySet().stream()
+        .sorted((m1, m2) -> Double.compare(m2.getValue(), m1.getValue()))
+        .mapToInt(HashMap.Entry::getKey)
+        .toArray();
     }
 }
