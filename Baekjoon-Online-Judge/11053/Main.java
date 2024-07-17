1bec045503fd30cf57c46d2d4deeaccfe2113149
jieun <92997924+JinjinBread@users.noreply.github.com>
2024-07-17 20:01:39 +0900

    study20: Baekjoon No.11053 DP로 수정

--- Baekjoon-Online-Judge/11053/Main.java
+++ Baekjoon-Online-Judge/11053/Main.java
@@ -4,9 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -22,41 +20,22 @@ public class Main {
             numArr[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 1. 수열을 오름 차순 정렬한다.
-        int[] numOrder = Arrays.copyOf(numArr, N);
-        List<Integer> orderList = Arrays.stream(numOrder).distinct().sorted().boxed().collect(Collectors.toList());
+        // DP(다이나믹 프로그래밍, 동적 계획법)를 사용
 
-        int[] numArrOrder = new int[N];
-        // 2. 원래 순서의 수열에 크기별 idx를 넣는다.
-        for (int i = 0; i < N; i++) {
-            int num = numArr[i];
-            numArrOrder[i] = orderList.indexOf(num);
-        }
+        int[] dp = new int[N];
+        Arrays.fill(dp, 1); // 모든 수를 1로 초기화 (최소 길이는 1이므로)
 
-        int maxLength = 1; // 최소 1
-        for (int i = 0; i < numArr.length; i++) {
-            // 모든 수열을 돌면서 첫 시작 지점으로 선택
-            // 첫 시작 지점일 때, 여러 개의 증가하는 부분 수열 중 가장 긴 부분 수열의 길이를 구함
-            maxLength = Math.max(calculateLength(numArr, numArrOrder, i), maxLength);
-        }
-
-        bw.write(String.valueOf(maxLength));
-        bw.flush();
-    }
-
-    private static int calculateLength(int[] numArr, int[] numArrOrder, int startIdx) {
-
-        int maxLength = 1;
-
-        for (int i = startIdx + 1; i < numArr.length; i++) {
-            
-            // i 번째에 있는 수가 현재 startIdx 보다 큰 수면 타고 들어감
-            if (numArrOrder[i] > numArrOrder[startIdx]) {
-                maxLength = Math.max(maxLength, 1 + calculateLength(numArr, numArrOrder, i));
+        // 변수 간의 관계식(점화식) 작성
+        // 특정 수를 가장 마지막 원소라고 가정한 부분 수열의 길이를 저장
+        // 맨 앞의 수부터 특정 수 보다 작으면 해당 수가 마지막 원소일 때의 부분 수열의 길이 + 1을 저장한다.
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) { // 특정 수 i번째의 수를 가장 마지막 원소라고 가정
+                if (numArr[i] > numArr[j])
+                    dp[i] = Math.max(dp[i], dp[j] + 1); // 더 긴 부분 수열의 길이를 저장
             }
         }
 
-        // startIdx가 제일 큰 수 이거나 제일 마지막에 있으면 1 리턴
-        return maxLength;
+        bw.write(String.valueOf(Arrays.stream(dp).max().getAsInt()));
+        bw.flush();
     }
 }
