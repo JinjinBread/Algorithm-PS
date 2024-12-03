42646432a23a0245b98b767a084846d290e3d0da
Jieun <92997924+JinjinBread@users.noreply.github.com>
2024-12-03 15:00:08 +0900

    [Silver I] Title: 정수 삼각형, Time: 232 ms, Memory: 27604 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] triangle = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < i+1; j++) {
+                triangle[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[][] dp = new int[n][n];
+
+        // 리프 노드를 dp에 저장(초기화)
+        for (int i = n-1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = triangle[i][j];
+            }
+        }
+
+        for (int i = n-2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[i][j] = triangle[i][j] + Math.max(dp[i+1][j], dp[i+1][j+1]);
+            }
+        }
+        
+        bw.write(String.valueOf(dp[0][0]));
+        bw.flush();
+    }
+}
